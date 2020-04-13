e87a313b9ff85814ac4d7202fe83ab1962b2799e
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-13 14:24:16 +0200

    Navigáció létrehozása kezdőlap-login-register oldalak között

--- protected/nav.php
+++ protected/nav.php
@@ -1,25 +1,26 @@
 <hr>
 
-<a href="index.php">Home</a>
-<span>&nbsp; | &nbsp;</span>
-<?php if(!IsUserLoggedIn()) : ?>
-    <a href="index.php?P=login">Login</a>
-    <span>&nbsp; | &nbsp;</span>
-    <a href="index.php?P=register">Register</a>
-<?php else : ?>
-
-    <a href="index.php?P=test">Permission Test</a>
-    <span>&nbsp; | &nbsp;</span>
-
-    <?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] >= 1) : ?>
-        <a href="index.php?P=users">User List</a>
-        <span>&nbsp; | &nbsp;</span>
-        <a href="index.php?P=addWorker">Add worker</a>
-        <span>&nbsp; | &nbsp;</span>
-
+<div class="collapse navbar-collapse" id="myNavbar">
+    <ul class="navbar-nav ml-auto">
+    <?php if(!IsUserLoggedIn()) : ?>
+        <li class="nav-item active">
+            <a class="nav-link" href="index.php">Kezdőlap<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?P=login">Bejelentkezés</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?P=register">Regisztráció</a>
+        </li>
+    <?php else : ?>
+        <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menü</a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown01">
+                <a class="dropdown-item" href="#">Valami</a>                                                               
+                <a class="dropdown-item" href="#">Valami 2</a>
+                <a class="dropdown-item" href="#">Valami 3</a>
+            </div>
+        </li>
     <?php endif; ?>
-
-    <a href="index.php?P=logout">Logout</a>
-<?php endif; ?>
-
-<hr>
+    </ul>
+</div>
