2313d775b8bc7f618af3f1267c66a9b681eeddff
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-15 15:36:23 +0200

    Adatbázis kapcsolat módosítása

--- protected/config.php
+++ protected/config.php
@@ -7,8 +7,8 @@ define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
 define('USER_MANAGER', PROTECTED_DIR.'userManager.php');
 
 define('DB_TYPE','mysql');
-define('DB_HOST','localhost');
-define('DB_NAME','eqdgrj');
+define('DB_HOST','localhost:3308');
+define('DB_NAME','hasznaltautok');
 define('DB_USER','root');
 define('DB_PASS','');
 define('DB_CHARSET','utf-8');
