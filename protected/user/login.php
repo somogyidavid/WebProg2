e8cf05fec446b958e42688d8c974404b57024673
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-15 15:34:33 +0200

    Bejelentkezési rendszer létrehozása és hibák kiírásának kezelése

--- protected/user/login.php
+++ protected/user/login.php
@@ -1,37 +1,56 @@
+<div class="container mt-5">
 <?php
+  require_once PROTECTED_DIR.'functions.php';
+  if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['successful_register'])){
+    DisplaySuccess("regisztráció!");
+  }
+
   if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])){
     $postData = [
       'email' => $_POST['email'],
       'password' => $_POST['password']
     ];
 
+    $errors = [];
+
     if(empty($postData['email']) || empty($postData['password'])){
-      echo "Hiányzó adat(ok)!";
-    } else if(!filter_var($postData['email'], FILTER_VALIDATE_EMAIL)){
-      echo "Hibás email formátum!";
-    } else if(!UserLogin($postData['email'], $postData['password'])){
-      echo "Hibás email cím vagy jelszó!";
+      $errors['general'][] = "Hiányzó adat(ok)!";
+    }
+    if(!filter_var($postData['email'], FILTER_VALIDATE_EMAIL)){
+      $errors['email'][] = "Hibás email formátum!";
+    }
+    if(!UserLogin($postData['email'], $postData['password'])){
+      $errors['wrongdata'][] = "Hibás email cím vagy jelszó!";
+    }
+
+    if(count($errors) == 0)
+      header('Location: index.php?P=home&successful_login=1');
     }
 
     $postData['password'] = "";
-  }
 ?>
+  <div class="text-center">
+  <?php
+    if(isset($errors['general'])){
+      echo "<div class='alert alert-danger text-center w-30'>".DisplayError('general')."</div>";
+    }
+  ?>
+  </div>
+</div>
 
-
-<div class="container login-container" style="background-color: white;">
+<div class="container mt-5 login-container" style="background-color: white;">
   <div class="login-form">
-    <form class="form-signin">
+    <form class="form-signin" method="post">
       <h1 class="h3 mb-3 text-center font-weight-normal">Bejelentkezés</h1>
-        <label for="inputEmail" class="sr-only" name="email">Email</label>
-          <input type="email" id="inputEmail" class="form-control mb-4" placeholder="Felhasználónév" required autofocus>
-        <label for="inputPassword" class="sr-only" name="password">Jelszó</label>
-          <input type="password" id="inputPassword" class="form-control" placeholder="Jelszó" required>
+          <input type="email" id="inputEmail" class="form-control mb-4" placeholder="Email" name="email">
+          <input type="password" id="inputPassword" class="form-control" placeholder="Jelszó" name="password">
+          <small class="text-danger"><?php echo DisplayError('wrongdata'); ?></small>
       <div class="checkbox mb-4">
           <label>
             <input type="checkbox" value="remember-me"> Felhasználónév megjegyzése
           </label>
         </div>
-            <button class="btn btn-lg btn-primary btn-block" type="submit">Bejelentkezés</button>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Bejelentkezés</button>
       </form>
   </div>
 </div>
