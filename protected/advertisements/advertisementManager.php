d05c552c0a507ceb0bedc4a332dc30c5cad5c514
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-23 12:16:26 +0200

    Hirdetés törlése már létező felvételi próbálkozás esetén

--- protected/advertisements/advertisementManager.php
+++ protected/advertisements/advertisementManager.php
@@ -1,46 +1,53 @@
 <?php
 require_once DATABASE_CONTROLLER;
 function insertAdvertisement($userid, $title, $licencePlate, $brand, $model, $vintage, $type, $condition, $price, $kilometer, $fuel, $engineCapacity, $color, $description, $contact){
-    $connection = getConnection();
-    $query = "INSERT INTO advertisements (userid, title) VALUES (:userid, :title)";
-    $params= [
-        ':userid' => $userid,
-        ':title' => $title
+    $checkQuery = "SELECT id FROM advertisementDetails WHERE licencePlate = :licencePlate";
+    $checkParams = [
+        ':licencePlate' => $licencePlate
     ];
-    $statement = $connection->prepare($query);
-    $success = $statement->execute($params);
+    $record = getRecord($checkQuery,$checkParams);
 
-    if($success){
-        $adID = $connection->lastInsertId();
-        $query = "INSERT INTO advertisementDetails(advertisementId,licencePlate,brand,model,vintage,type,`condition`,price,kilometer,fuel,engineCapacity,color,description,contact) 
-            VALUES (:advertisement_id, :licence_plate, :brand, :model, :vintage, :type, :condition, :price, :kilometer, :fuel, :engine_capacity, :color, :description, :contact)";
-        $params = [
-            ':advertisement_id' => $adID,
-            ':licence_plate' => $licencePlate,
-            ':brand' => $brand,
-            ':model' => $model,
-            ':vintage' => $vintage,
-            ':type' => $type,
-            ':condition' => $condition,
-            ':price' => $price,
-            ':kilometer' => $kilometer,
-            ':fuel' => $fuel,
-            ':engine_capacity' => $engineCapacity,
-            ':color' => $color,
-            ':description' => $description,
-            ':contact' => $contact
+    if(empty($record)){
+        $connection = getConnection();
+        $query = "INSERT INTO advertisements (userid, title) VALUES (:userid, :title)";
+        $params= [
+            ':userid' => $userid,
+            ':title' => $title
         ];
-
         $statement = $connection->prepare($query);
         $success = $statement->execute($params);
+
         if($success){
-            // KÉSZ
-        }else{
-            var_dump($adID);
-            var_dump($statement->errorInfo());
+            $adID = $connection->lastInsertId();
+            $query = "INSERT INTO advertisementDetails(advertisementId,licencePlate,brand,model,vintage,type,`condition`,price,kilometer,fuel,engineCapacity,color,description,contact) 
+                VALUES (:advertisement_id, :licence_plate, :brand, :model, :vintage, :type, :condition, :price, :kilometer, :fuel, :engine_capacity, :color, :description, :contact)";
+            $params = [
+                ':advertisement_id' => $adID,
+                ':licence_plate' => $licencePlate,
+                ':brand' => $brand,
+                ':model' => $model,
+                ':vintage' => $vintage,
+                ':type' => $type,
+                ':condition' => $condition,
+                ':price' => $price,
+                ':kilometer' => $kilometer,
+                ':fuel' => $fuel,
+                ':engine_capacity' => $engineCapacity,
+                ':color' => $color,
+                ':description' => $description,
+                ':contact' => $contact
+            ];
+
+            $statement = $connection->prepare($query);
+            $success = $statement->execute($params);
+            if($success){
+                return true;
+            }else{
+                $query = "DELETE FROM advertisements WHERE id = :id";
+                $$params = [ ':id' => $adID];
+                return false;
+            }
         }
     }
-    
-
 }
 ?>
