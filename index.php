b9571bef9e14109006ae7e9d5158d69fdb44a6ff
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-25 21:38:56 +0200

    Lehetséges márka-model kombinációk script hozzáadva

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Beadandó</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="public/css/login.css">
+    <link rel="stylesheet" href="public/css/design.css">
     <script src="jquery-3.5.0.min.js" type="text/javascript"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
