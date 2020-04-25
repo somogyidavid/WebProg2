4bb1b2eca7e58d249073e404592f0b9c5d6df146
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-25 21:29:26 +0200

    Design módosítások és javítások

--- protected/advertisements/advertisementDetails.php
+++ protected/advertisements/advertisementDetails.php
@@ -1,14 +1,23 @@
+<div class="container">
 <?php
+    if(!isset($_GET['id']) || empty($_GET['id']) || !isset($_GET['uid']) || empty($_GET['uid'])) header('Location: index.php');
     require_once DATABASE_CONTROLLER;
+    require_once PROTECTED_DIR.'functions.php';
+
+    if($_SERVER['REQUEST_METHOD'] == 'GET' && array_key_exists('successful_update',$_GET) && isset($_GET['successful_update']) && IsUserLoggedIn()){
+        DisplaySuccess("módosítás!");
+    }
+
     if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])){
         $id = $_GET['id'];
-        $query = "SELECT ad.id, ad.title, det.licencePlate, brands.brand_name, models.model_name, det.vintage, det.type, det.condition, det.price, det.kilometer, det.fuel, det.engineCapacity, det.color, det.description, det.contact, det.image FROM advertisements ad INNER JOIN advertisementdetails det ON ad.id = det.advertisementId INNER JOIN brands ON det.brand = brands.id INNER JOIN models ON det.model = models.id WHERE ad.id=:id";
+        $query = "SELECT ad.id, ad.userId, ad.title, det.licencePlate, brands.brand_name, models.model_name, det.vintage, det.type, det.condition, det.price, det.kilometer, det.fuel, det.engineCapacity, det.color, det.description, det.contact, det.image FROM advertisements ad INNER JOIN advertisementdetails det ON ad.id = det.advertisementId INNER JOIN brands ON det.brand = brands.id INNER JOIN models ON det.model = models.id WHERE ad.id=:id";
         $params = [
             ':id' => $id
         ];
         $details = getRecord($query,$params);
     }
 ?>
+</div>
 
 <div class="container bg-white mt-2 pb-0 mt-5 text-center">
     <h1 class="pb-2"><?=$details['title']?></h1>
@@ -17,17 +26,20 @@
         <div class="row">
             <div class="col-md-6 text-center mt-4">
                 <img class="img-thumbnail mb-0 mt-3 border border-dark border-2" src="public/images/<?=$details['image']?>" width=535>
-                <div class="form-row">
-                    <div class="form-group col-md-4">
-                        <a href="index.php?P=home" class="btn btn-primary mt-2">Vissza</a>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <a href="#" class="btn btn-primary mt-2">Szerkesztés</a>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <a href="#" class="btn btn-primary mt-2">Törlés</a>
-                    </div>
+ 
+                <div class="form-row pt-4">
+                    <table class="table table-dark table-striped">
+                        <tbody>
+                            <tr>
+                                <td class="font-weight-bold">Leírás</td>
+                            </tr>
+                            <tr>
+                                <td><?=$details['description']?></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
+
             </div>
             <div class="col-md-6">
                 <div class="container mt-4">
@@ -73,16 +85,29 @@
                             <th scope="row">Szín:</th>
                             <td><?=$details['color']?></td>
                             </tr>
-                            <tr>
+                            <!--<tr>
                             <th scope="row">Leírás:</th>
                             <td><?=$details['description']?></td>
-                            </tr>
+                            </tr>-->
                             <tr>
                             <th scope="row">Elérhetőség:</th>
                             <td><?=$details['contact']?></td>
                             </tr>
                         </tbody>
                     </table>
+                    <div class="form-row text-center">
+                        <div class="form-group col-md-4">
+                            <a href="javascript:history.go(-1)" class="btn btn-dark mt-2">Vissza</a>
+                        </div>
+                        <?php if($_SESSION['permission'] >= 1 || $_SESSION['uid'] == $_GET['uid']) : ?>
+                        <div class="form-group col-md-4">
+                            <a href="index.php?P=updateAdvertisement&id=<?=$details['id']?>&uid=<?=$details['userId']?>" class="btn btn-dark mt-2">Szerkesztés</a>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <a href="index.php?P=removeAdvertisement&id=<?=$details['id']?>&uid=<?=$details['userId']?>" class="btn btn-dark mt-2">Törlés</a>
+                        </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
         </div>
