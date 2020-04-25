2be388d398bdb8900a4e7429a449a157cb9ba17c
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-25 21:35:12 +0200

    Hirdetés műveletekhez kapcsolódó routingok hozzáadva

--- protected/routing.php
+++ protected/routing.php
@@ -6,12 +6,17 @@
             case 'home': require_once PROTECTED_DIR.'normal/home.php';break;
             case 'test': require_once PROTECTED_DIR.'normal/permissionTest.php';break;
 
-            case 'addWorker': IsUserLoggedIn() ? require_once PROTECTED_DIR.'worker/add.php' : header('Location: index.php'); break;
+            case 'addAdvertisement': IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/addAdvertisement.php' : header('Location: index.php'); break;
+            case 'advertisementDetails' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/advertisementDetails.php' : header('Location: index.php'); break;
+            case 'userAdvertisements' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/userAdvertisements.php' : header('Location: index.php'); break;
+            case 'removeAdvertisement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/removeAdvertisement.php' : header('Location: index.php'); break;
+            case 'updateAdvertisement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/updateAdvertisement.php' : header('Location: index.php'); break;
 
             case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
             case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
             case 'logout': IsUserLoggedIn() ? UserLogOut() : header('Location: index.php'); break;
             case 'users': IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/userList.php' : header('Location: index.php'); break;
+
             default: require_once PROTECTED_DIR.'normal/404.php';break;
         }
 
