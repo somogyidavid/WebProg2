3baa711a6d431a7b995083ef0c9615ed8315fa10
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-04-25 21:33:53 +0200

    Értesítések hozzáadva

--- protected/normal/home.php
+++ protected/normal/home.php
@@ -1,3 +1,14 @@
 <div class="container">
-    HOME
+    <?php
+        require_once PROTECTED_DIR.'functions.php';
+        if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['successful_login']) && IsUserLoggedIn()){
+            DisplaySuccess("bejelentkezés!");
+        }
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['successful_ad_insert']) && IsUserLoggedIn()){
+            DisplaySuccess("hirdetésfeladás!");
+        }
+
+        require_once PROTECTED_DIR.'advertisements/listAdvertisements.php';
+    ?>
 </div>
