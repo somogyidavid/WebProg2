670d6735033e51a0c622a9184f2b1325242b5e78
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-05-04 10:20:28 +0200

    Kereséshez tartozó routing hozzáadva

--- protected/routing.php
+++ protected/routing.php
@@ -11,11 +11,16 @@
             case 'userAdvertisements' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/userAdvertisements.php' : header('Location: index.php'); break;
             case 'removeAdvertisement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/removeAdvertisement.php' : header('Location: index.php'); break;
             case 'updateAdvertisement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/updateAdvertisement.php' : header('Location: index.php'); break;
+            case 'advertisementManagement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/advertisementManagement.php' : header('Location: index.php'); break;
+            case 'searchAdvertisement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'advertisements/searchAdvertisement.php' : header('Location: index.php'); break;
 
             case 'login': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/login.php' : header('Location: index.php'); break;
             case 'register': !IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/register.php' : header('Location: index.php'); break;
             case 'logout': IsUserLoggedIn() ? UserLogOut() : header('Location: index.php'); break;
-            case 'users': IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/userList.php' : header('Location: index.php'); break;
+            case 'profile': IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/profile.php' : header('Location: index.php'); break;
+            case 'updateUser' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/updateUser.php' : header('Location: index.php'); break;
+            case 'removeUser' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/removeUser.php' : header('Location: index.php'); break;
+            case 'userManagement' : IsUserLoggedIn() ? require_once PROTECTED_DIR.'user/userManagement.php' : header('Location: index.php'); break; 
 
             default: require_once PROTECTED_DIR.'normal/404.php';break;
         }
