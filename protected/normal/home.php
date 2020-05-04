acc08a4d78d3046a13f800c1e35940223299930a
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-05-04 10:16:09 +0200

    Home oldal megjelenítendő üzenetek hozzáadva

--- protected/normal/home.php
+++ protected/normal/home.php
@@ -9,6 +9,14 @@
             DisplaySuccess("hirdetésfeladás!");
         }
 
+        if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['successful_ad_remove']) && IsUserLoggedIn()){
+            DisplaySuccess("hirdetés törlés!");
+        }
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['successful_user_remove'])){
+            DisplaySuccess("felhasználó törlés!");
+        }
+
         require_once PROTECTED_DIR.'advertisements/listAdvertisements.php';
     ?>
 </div>
