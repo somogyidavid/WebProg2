d1c64306c18ae01cc97df7fa2980b81c7994ec07
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-05-04 10:19:48 +0200

    Keresés gomb és linkek hozzáadva

--- protected/nav.php
+++ protected/nav.php
@@ -1,9 +1,8 @@
 <hr>
-
 <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="navbar-nav ml-auto">
     <li class="nav-item active">
-            <a class="nav-link" href="index.php?P=home">Kezdőlap<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php?P=home">Kezdőlap<span class="sr-only">(current)</span></a>
     </li>
     <?php if(!IsUserLoggedIn()) : ?>
 
@@ -14,25 +13,26 @@
             <a class="nav-link" href="index.php?P=register">Regisztráció</a>
         </li>
     <?php else : ?>
+        <li class="nav-item">
+            <a class="nav-link" href="index.php?P=searchAdvertisement">Keresés</a>
+        </li>
         <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Menü</a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="#">Hirdetés feladása</a>                                                               
-                <a class="dropdown-item" href="#">Saját hirdetéseim</a>
+                <a class="dropdown-item" href="index.php?P=addAdvertisement">Hirdetés feladása</a>                                                               
+                <a class="dropdown-item" href="index.php?P=userAdvertisements&uid=<?=$_SESSION['uid']?>">Saját hirdetéseim</a>
             <?php if(!isset($_SESSION['permission']) || $_SESSION['permission'] >= 1) : ?>
-                <a class="dropdown-item" href="#">Hirdetések kezelése</a>
-                <a class="dropdown-item" href="#">Profilok kezelése</a>
+                <a class="dropdown-item" href="index.php?P=advertisementManagement">Hirdetések kezelése</a>
+                <a class="dropdown-item" href="index.php?P=userManagement">Profilok kezelése</a>
             <?php endif; ?>
-                <a class="dropdown-item" href="#">Work in progress...</a>
             </div>
         </li>
         
         <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profil</a>
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdown01">
-                <a class="dropdown-item" href="#">Saját profil megtekintése</a>                                                               
-                <a class="dropdown-item" href="#">Profil szerkesztése</a>
-                <a class="dropdown-item" href="#"></a>
+                <a class="dropdown-item" href="index.php?P=profile&uid=<?=$_SESSION['uid']?>">Saját profil megtekintése</a>                                                               
+                <a class="dropdown-item" href="index.php?P=updateUser&uid=<?=$_SESSION['uid']?>">Profil szerkesztése</a>
             </div>
         </li>
 
