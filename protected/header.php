32eea0a50863e96304b6361f2f61838b3f2c2ca7
Somogyi Dávid <ksomogyi1515@gmail.com>
2020-05-04 10:19:04 +0200

    Header design hozzáadva

--- protected/header.php
+++ protected/header.php
@@ -1,1 +1,13 @@
-HEADER
+<!DOCTYPE html>
+<html lang="en">
+<body style="background-color: #8EA3BF;">
+    
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="index.php">Használt autók</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <?php include_once PROTECTED_DIR.'nav.php' ?>
+      </nav>
+</html>
